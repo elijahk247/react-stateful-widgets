6431bf0c3ca2ac306e2b246cc39bc25fa3b3629d
Elijah Kim <elijahkim@Elijahs-MacBook-Pro.local>
2020-08-11 19:30:29 -0400

    Fixed all bugs

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -11,6 +11,7 @@ We can only feature one awesome programmer at a time.
 Find comments below to help you along.
 */
 
+
 import React, { useState } from 'react';
 
 // Use this variable ONLY to initialize a slice of state!
@@ -27,7 +28,7 @@ const listOfAwesome = [
 export default function Programmers() {
   // We'll have to use the state hook twice, as we need two slices of state.
   // The programmers on the one hand, and the id of the featured programmer on the other.
-  const [awesome, setAwesome] = useState(listOfAwesome)
+  const [awesome] = useState(listOfAwesome)
   const [activeAwesome, setActiveAwesome] = useState(null)
 
   const getNameOfFeatured = () => {
@@ -36,22 +37,20 @@ export default function Programmers() {
     // Using the currently celebrated id, find inside the programmers slice of state
     // the _name_ of the currently celebrated programmer, and return it.
 
-    console.log(activeAwesome.name)
-    console.log('hello');
+    console.log(activeAwesome)
+
     console.log(typeof(activeAwesome))
     console.log(Number(activeAwesome));
-    console.log(awesome[Number(activeAwesome)].name);
+    console.log(awesome[Number(activeAwesome) - 1].name);
     console.log(awesome)
 
     const index = Number(activeAwesome) - 1;
     console.log(index)
-    /*
-    return awesome.find(p => {
-      return p.id - 1 === activeAwesome;
-    })?.name*/
+    
+    //return awesome.find(p => p.id === activeAwesome)?.name
     return `${awesome[index].name}`
   };
-
+ 
 
   const style = {
     fontSize: '1.5em',
@@ -67,6 +66,7 @@ export default function Programmers() {
           /* Nasty bug! We should map over a slice of state, instead of 'listOfAwesome'.
           We might say: "it works, though!" But if the list of programmers is not state,
           we could never add or edit programmers in the future. The list would be a static thing. ;)" */
+          
           awesome.map(dev =>
             <div key={dev.id}>
               {dev.name} <button onClick={() => { /* set the featured id passing dev.id */
@@ -78,10 +78,75 @@ export default function Programmers() {
       {
         // Ternaries are fantastic to render "one thing or the other" depending on the "truthiness" of something.
         // Pseudo-code: if the currently featured id is truthy render div 1, otherwise render div 2. Fix!
-        activeAwesome !== null
+        //activeAwesome !== null
+        activeAwesome
           ? <div style={style}>🎉 Let&apos;s celebrate {getNameOfFeatured()}! 🥳</div>
           : <div style={style}>Pick an awesome programmer</div>
       }
     </div>
   );
 }
+
+
+/*
+PROGRAMMERS Instructions
+Watch this short video:
+https://tk-assets.lambdaschool.com/1ea6e6a2-2ef1-45de-bcab-b99a8f775870_programmers.gif
+This component keeps track of a list of pioneers in the field of programming on the one hand,
+and the id of the currently celebrated programmer on the other. That's two slices of state!
+We can only feature one awesome programmer at a time.
+Find comments below to help you along.
+*/
+// import React, { useState } from 'react';
+// // Use this variable ONLY to initialize a slice of state!
+// // There is something in the JSX right now breaking this rule.
+// const listOfAwesome = [
+//   { id: '1', name: 'Ada Lovelace' },
+//   { id: '2', name: 'Grace Hopper' },
+//   { id: '3', name: 'Evelyn Boyd Granville' },
+//   { id: '4', name: 'Mary Kenneth Keller' },
+//   { id: '5', name: 'Frances Allen' },
+//   { id: '6', name: 'Carol Shaw' },
+// ];
+// export default function Programmers() {
+//   // We'll have to use the state hook twice, as we need two slices of state.
+//   // The programmers on the one hand, and the id of the featured programmer on the other.
+//   const [programmers] = useState(listOfAwesome);
+//   const [featured, setFeatured] = useState(null);
+//   const getNameOfFeatured = () => {
+//     // This is not an event handler but a helper function. See its usage below.
+//     // It's going to need information from both slices of state!
+//     // Using the currently celebrated id, find inside the programmers slice of state
+//     // the _name_ of the currently celebrated programmer, and return it.
+//     return programmers.find(pr => pr.id === featured)?.name;
+//   };
+//   const style = {
+//     fontSize: '1.5em',
+//     marginTop: '0.5em',
+//     color: featured ? 'gold' : 'royalblue', // :thinking_face:
+//   };
+//   return (
+//     <div className='widget-programmers container'>
+//       <h2>Programmers</h2>
+//       <div className='programmers'>
+//         {
+//           /* Nasty bug! We should map over a slice of state, instead of 'listOfAwesome'.
+//           We might say: "it works, though!" But if the list of programmers is not state,
+//           we could never add or edit programmers in the future. The list would be a static thing. ;)" */
+//           programmers.map(dev =>
+//             <div key={dev.id}>
+//               {dev.name} <button onClick={() => { setFeatured(dev.id) /* set the featured id passing dev.id */ }}>Feature</button>
+//             </div>
+//           )
+//         }
+//       </div>
+//       {
+//         // Ternaries are fantastic to render "one thing or the other" depending on the "truthiness" of something.
+//         // Pseudo-code: if the currently featured id is truthy render div 1, otherwise render div 2. Fix!
+//         featured
+//           ? <div style={style}>:tada: Let&apos;s celebrate {getNameOfFeatured()}! 🥳</div>
+//           : <div style={style}>Pick an awesome programmer</div>
+//       }
+//     </div>
+//   );
+// }
